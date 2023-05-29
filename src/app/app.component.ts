7e628c7547828c3b273cc7554565857a7a24adbe
Paweł Kubiak <pawel.kubiak.dev@gmail.com>
2023-05-29 22:59:17 +0100

    feat: implement layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterOutlet]
+  imports: [CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    RouterLink,
+    RouterOutlet]
 })
 export class AppComponent {
   title = 'directives-in-practice';
+
+  pages$ = of([
+    {
+      title: 'temp',
+      url: 'temp',
+      label: 'temp',
+      icon: 'temp',
+    }
+  ]);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/routes';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes),
+    importProvidersFrom([BrowserModule, BrowserAnimationsModule]), provideAnimations()],
 });
